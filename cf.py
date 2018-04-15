8d48553f594f73fc7cfda4281776622aca15e3e8
Jaehun Kim <jaehun.j.kim@gmail.com>
2018-04-16 01:06:50 +0200

    update lambda bprmf

--- cf.py
+++ cf.py
@@ -29,7 +29,17 @@ except:
 
 def sigmoid(x):
     """"""
-    return 1. / (1. + np.exp(-x))
+    return 1. / (1. + np.e ** -x)
+
+
+def p_bar(X, u, i, j):
+    """ posterior prob (true label) for u, i, j """
+    if X[u, i] < X[u, j]:
+        return 0
+    elif X[u, i] > X[u, j]:
+        return 1
+    else:
+        return .5
 
 
 @background(max_prefetch=10)
@@ -128,7 +138,7 @@ class BPRMF:
 
         return Y
 
-    def predict(self, u, k=500):
+    def predict_k(self, u, k=500):
         """"""
         r = self.U[u].matmul(self.V.t())
         _, ind = torch.sort(r)
@@ -216,7 +226,7 @@ class BPRMF:
             print('[Warning] User stopped the training!')
 
 
-class BPRMF_numpy:
+class BPRMFcpu:
     """
     MF based on BPR loss.
     only covers SGD (m==1)
@@ -237,7 +247,7 @@ class BPRMF_numpy:
         self.report_every = report_every
         self.verbose = verbose
 
-    def predict(self, u, k=500):
+    def predict_k(self, u, k=500):
         """"""
         r = self.U[u].dot(self.V.T) + self.b_i
         return np.argsort(r)[::-1][:k]
@@ -385,7 +395,216 @@ class BPRMF_numpy:
                             ndcg = []
                             for u_ in rnd_u:
                                 true = sp.find(val[u_])[1]
-                                pred = self.predict(u_, k=500)
+                                pred = self.predict_k(u_, k=500)
+
+                                rprec.append(r_precision(true, pred))
+                                ndcg.append(NDCG(true, pred))
+                            rprec = filter(lambda r: r is not None, rprec)
+                            ndcg = filter(lambda r: r is not None, ndcg)
+                            self.acc_curve.append(
+                                (np.mean(rprec), np.mean(ndcg)))
+
+                    # update
+                    self.update(u, i, j)
+                    self.k += 1
+
+        except KeyboardInterrupt:
+            print('[Warning] User stopped the training!')
+
+
+class LambdaBPRMF:
+    """"""
+    def __init__(self, n_components, alpha=0.001, beta=0.1, eps=1,
+                 init_factor=1e-1, n_epoch=1, report_every=100,
+                 prepare_psi_upto=100, verbose=True):
+        """"""
+        self.n_components_ = n_components
+        self.n_epoch = n_epoch
+        self.alpha = alpha  # learning rate
+        self.beta = beta  # regularization weight
+        self.eps = eps
+        self.init_factor = init_factor  # init weight
+        self.loss_curve = []
+        self.acc_curve = []
+        self.report_every = report_every
+        self.verbose = verbose
+        self.prepare_psi_upto = prepare_psi_upto
+
+    def predict_k(self, u, k=500):
+        """"""
+        r = self.U[u].dot(self.V.T) + self.b_i
+        return np.argsort(r)[::-1][:k]
+
+    def predict(self, u, i):
+        """"""
+        r = self.U[u].dot(self.V[i]) + self.b_i[i]
+        return r
+
+    def p(self, u, i, j):
+        """"""
+        a_i = self.predict(u, i)
+        a_j = self.predict(u, j)
+        return sigmoid(a_i - a_j)
+
+    def forward(self, u, i, j):
+        """"""
+        U, I, J = self.U[u], self.V[i], self.V[j]
+        b_i = self.b_i[i]
+        b_j = self.b_i[j]
+        x_ui = np.inner(U, I)
+        x_uj = np.inner(U, J)
+        x_ = b_i - b_j + x_ui - x_uj
+
+        L = np.log(1. / (1. + np.e ** (-x_)))
+        L -= self.beta * (np.sum(U**2) + np.sum(I**2) + np.sum(J**2))
+        L -= self.beta * .1 * np.sum(self.b_i[i]**2) + np.sum(self.b_i[j]**2)
+        return L
+
+    def update(self, u, i, j):
+        """"""
+        U, I, J = self.U[u], self.V[i], self.V[j]
+        b_i = self.b_i[i]
+        b_j = self.b_i[j]
+        x_ui = np.inner(U, I)
+        x_uj = np.inner(U, J)
+        x_ = b_i - b_j + x_ui - x_uj
+
+        # calc psi (lambda weight)
+        if int(self.T) in self.psi:
+            psi = self.psi[int(self.T)]
+        else:
+            upto = int((self.n_items - 1.) / self.T) + 1
+            psi = sum([1./(r+1.) for r in xrange(upto)]) / self.gamma_I
+
+        # get grad (calc lambda)
+        a = 1. / (1. + np.e ** (x_))
+        a *= psi
+        di = a - self.beta * b_i
+        dj = -a - self.beta * b_j
+        dU = a * (I - J) - self.beta * U
+        dI = a * U - self.beta * I
+        dJ = a * (-U) - self.beta * J
+
+        # SGD
+        self.b_i[i] += self.alpha * di
+        self.b_i[j] += self.alpha * dj
+        self.U[u] += self.alpha * dU
+        self.V[i] += self.alpha * dI
+        self.V[j] += self.alpha * dJ
+
+    @background(max_prefetch=10)
+    def batch_prep_lambda_scheme(self, X, x_trp):
+        """"""
+        M = X.shape[0]
+        N = len(x_trp)  # number of records
+        self.n_items = X.shape[1]
+        self.n_users = M
+
+        if self.verbose:
+            it = tqdm(xrange(N), total=N, ncols=80)
+        else:
+            it = xrange(N)
+
+        for n in it:
+            # select user
+            u = np.random.choice(M)
+            i = sp.find(X[u])[1]
+            if len(i) == 0:
+                continue
+            pos_i = set(i)
+            i = np.random.choice(i)  # positive samples
+            y_i = self.predict(u, i)
+
+            # sample negative sample
+            self.T = 1
+            j = np.random.choice(self.n_items)
+            y_j = self.predict(u, j)
+            while (
+                ((y_i - y_j > self.eps) or (p_bar(X, u, i, j) != 1)) and
+                (self.T < (self.n_items - 1.))):
+
+                j = np.random.choice(self.n_items)
+                y_j = self.predict(u, j)
+                self.T += 1
+
+            if (y_i - y_j <= self.eps) and (p_bar(X, u, i, j) == 1):
+                pass
+            else:
+                continue
+
+            yield u, i, j
+
+    def _preproc_data(self, X):
+        """ unwrap sparse matrix X into triplets """
+        Y = sparse2triplet(X)  # implement data processing
+        return Y
+
+    def fit(self, X, val=None):
+        """"""
+        # quick convert
+        X = X.tocsr()
+
+        # calc gamma_I
+        self.gamma_I = sum([1./(r+1.) for r in xrange(X.shape[1])])
+
+        # setup relevant hyper params
+        self.n_users = X.shape[0]
+        self.n_items = X.shape[1]
+        r = self.n_components_
+
+        # prepare psi per T
+        self.psi = {}
+        for t in xrange(1, self.prepare_psi_upto):
+            upto = int((self.n_items - 1.) / t) + 1
+            self.psi[t] = \
+                    sum([1./(r_+1.) for r_ in xrange(upto)]) / self.gamma_I
+
+        # init parameters
+        self.b_u = None
+        self.b_i = np.zeros((self.n_items,))
+
+        self.U = np.random.randn(self.n_users, r) * self.init_factor
+        self.V = np.random.randn(self.n_items, r) * self.init_factor
+        self.U_m, self.U_v = np.zeros(self.U.shape), np.zeros(self.U.shape)
+        self.V_m, self.V_v = np.zeros(self.V.shape), np.zeros(self.V.shape)
+        self.b_m, self.b_v = np.zeros(self.n_items), np.zeros(self.n_items)
+
+        # preprocess dataset
+        Y = self._preproc_data(X)
+
+        # training loop
+        if self.verbose:
+            N = trange(self.n_epoch, ncols=80)
+        else:
+            N = xrange(self.n_epoch)
+
+        self.k = 1
+        try:
+            for n in N:
+                for u, i, j in self.batch_prep_lambda_scheme(X, Y):
+                    if self.k % self.report_every == 0:
+                        # forward pass
+                        l = self.forward(u, i, j)
+
+                        # save loss curve
+                        self.loss_curve.append(l)
+
+                        # diplay loss
+                        if self.verbose:
+                            N.set_description(
+                                '[loss : {:.4f}]'.format(l)
+                            )
+
+                    if val is not None:
+                        if self.k % (self.report_every * 1e+2) == 0:
+                            rnd_u = np.random.choice(val.shape[0],
+                                                     int(val.shape[0] * 0.05),
+                                                     replace=False)
+                            rprec = []
+                            ndcg = []
+                            for u_ in rnd_u:
+                                true = sp.find(val[u_])[1]
+                                pred = self.predict_k(u_, k=500)
 
                                 rprec.append(r_precision(true, pred))
                                 ndcg.append(NDCG(true, pred))
@@ -427,7 +646,7 @@ class WRMF:
         self.loss_curve = []
         self.verbose = verbose
 
-    def predict(self, u, k=500):
+    def predict_k(self, u, k=500):
         """"""
         r = self.U[u].dot(self.V.T)
         return np.argsort(r)[::-1][:k]
@@ -469,7 +688,8 @@ def main(data_fn):
     # fit
     # model = BPRMF(10, alpha=0.003, beta=0.001, verbose=True)
     # model = WRMF(10, beta=1e-1, verbose=True)
-    model = BPRMF_numpy(10, alpha=0.004, beta=10, n_epoch=2, verbose=True)
+    # model = BPRMFcpu(10, alpha=0.004, beta=10, n_epoch=2, verbose=True)
+    model = LambdaBPRMF(10, alpha=0.1, beta=0.001, n_epoch=2, verbose=True)
     model.fit(d, dt)
 
     print('Evaluate!')
@@ -480,7 +700,7 @@ def main(data_fn):
     ndcg = []
     for u in tqdm(rnd_u, total=len(rnd_u), ncols=80):
         true = sp.find(dt[u])[1]
-        pred = model.predict(u, k=cutoff)
+        pred = model.predict_k(u, k=cutoff)
 
         rprec.append(r_precision(true, pred))
         ndcg.append(NDCG(true, pred))
